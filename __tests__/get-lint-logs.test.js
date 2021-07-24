41a0c304113f50abd980e235e829b1d039e976a8
sosukesuzuki <aosukeke@gmail.com>
2021-07-24 10:46:11 +0900

    Add tests for log

---
+++ __tests__/get-lint-logs.test.js
@@ -0,0 +1,48 @@
+const chalk = require("chalk");
+const { getLintLog } = require("../src/get-lint-log");
+const { runLint } = require("../src/run-lint");
+
+describe("get linting log", () => {
+  it("returns log text from linting errors", async () => {
+    const invalidWorkflowCode = `hogehoge  `;
+    const results = (await runLint(invalidWorkflowCode, "test.yml")).map(
+      (result) => {
+        return {
+          path: "test.yml",
+          data: invalidWorkflowCode,
+          ...result,
+        };
+      }
+    );
+    const text = getLintLog(results);
+    //     console.log(`${text}`);
+    expect(text).toBe(
+      chalk.yellow("test.yml") +
+        chalk.gray(":") +
+        1 +
+        chalk.gray(":") +
+        1 +
+        " " +
+        chalk.bold.white(
+          "workflow is scalar node but mapping node is expected"
+        ) +
+        " " +
+        chalk.gray("[ syntax-check ]") +
+        "\n" +
+        "> 1 | hogehoge  \n" +
+        "    | ^\n" +
+        chalk.yellow("test.yml") +
+        chalk.gray(":") +
+        1 +
+        chalk.gray(":") +
+        1 +
+        " " +
+        chalk.bold.white('"jobs" section is missing in workflow') +
+        " " +
+        chalk.gray("[ syntax-check ]") +
+        "\n" +
+        "> 1 | hogehoge  \n" +
+        "    | ^\n"
+    );
+  });
+});
